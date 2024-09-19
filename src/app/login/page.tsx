c038c545fecd681b4f724584d9f39fb0e9f22f02
kaliganoff <kaliganoff@yahoo.com>
2024-09-20 00:48:33 +0300

    feat: Create the Login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+import { LoginForm } from "@/components/LoginForm/LoginForm";
+
+export default function Login() {
+  return <div className="flex flex-row min-h-screen justify-center items-center">
+      <LoginForm />
+  </div>;
+}
